0d7b09180daab979351077d2f4eebdf5bbc490c8
Samandar <samandarahadjonov@gmail.com>
2022-12-04 16:52:14 +0500

    architecture built

---
+++ lib/components/routes/routes.dart
@@ -0,0 +1,29 @@
+import 'package:car_showroom/ui/home/detailes_page.dart';
+import 'package:car_showroom/ui/home/home_page.dart';
+import 'package:car_showroom/ui/home/splash_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RouteName{
+  static const home="home";
+  static const splash="splash";
+  static const detailes="detailes";
+}
+
+class AppRoutes{
+
+  static Route generateRote(RouteSettings settings){
+    dynamic args=settings.arguments;
+
+    switch(settings.name){
+      case RouteName.home:
+        return MaterialPageRoute(builder: (_)=>const HomePage());
+      case RouteName.splash:
+        return MaterialPageRoute(builder: (_)=>const SplashPage());
+      case RouteName.detailes:
+        return MaterialPageRoute(builder: (_)=>const DetailesPage());
+      default: return MaterialPageRoute(builder: (_)=>const Scaffold());
+    }
+
+  }
+}
